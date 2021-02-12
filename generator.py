c0dfcff7b9e611eae05627f7c2f7759dd5fb742c
hainesma <hainesma@gmail.com>
2021-02-12 00:30:19 -0500

    complete basic SVG house logo

--- generator.py
+++ generator.py
@@ -23,6 +23,28 @@ for f in glob.iglob('book/*.md'):
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<svg width="300" height="200">
+<polyline points="
+25 75
+25 165
+145 165
+145 55
+"></polyline>
+<polyline points="
+5 95
+95 5
+165 75
+"></polyline>
+<polyline points="
+55 45
+55 0
+65 0
+65 35
+"></polyline>
+<rect x="45" y="98" width="20" height="67"></rect>
+<rect x="95" y="98" width="20" height="25"></rect>
+<rect x="70" y="45" width="18" height="30"></rect>
+</svg>
 ''')
         file.write(html)
         file.write(r'''
