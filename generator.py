222d342d400126005677835851f2e298a3000656
hainesma <hainesma@gmail.com>
2021-03-01 00:23:05 -0500

    font

--- generator.py
+++ generator.py
@@ -20,9 +20,11 @@ for f in glob.iglob('book/*.md'):
 <head>
   <meta charset="utf-8" />
   <title>hainesma</title>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans&display=swap' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<header>
 <svg width="300" height="200">
 <polyline points="
 25 75
@@ -37,16 +39,17 @@ for f in glob.iglob('book/*.md'):
 "></polyline>
 <polyline points="
 55 45
-55 0
-65 0
+55 2
+65 2
 65 35
 "></polyline>
-<rect x="45" y="98" width="20" height="67"></rect>
-<rect x="95" y="98" width="20" height="25"></rect>
+<rect x="45" y="98" width="23" height="67"></rect>
+<rect x="100" y="98" width="20" height="25"></rect>
 <rect x="70" y="45" width="18" height="30"></rect>
 </svg>
 ''')
         file.write(html)
         file.write(r'''
+</header>
 </body>
 </html>''')
